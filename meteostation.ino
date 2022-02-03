b4c8f2d91338cedbbac7bf8971a562adfb5996ff
Med.Amine.Abdi <mohamedamine.abdi@ieee.org>
2022-02-03 10:42:52 +0100

    adding check value

--- meteostation.ino
+++ meteostation.ino
@@ -8,6 +8,10 @@
    USER VARIABLES
  ******************************************************************************/
 
+uint32_t messageRate = 60000;   // stores the current data message rate in Milliseconds
+unsigned long lastMessageTime = 0;   // stores the time when last data message was sent   1->1000 3600->3600000
+float h,t ; 
+
 #define DHTPIN 15    // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT11   // DHT 11
 DHT dht(DHTPIN, DHTTYPE);// Initialize DHT sensor.
@@ -27,10 +31,23 @@ void setup() {
 }
 
 void loop() {
-   Serial.println("Sampling data");
-   lo.addToPayload("Temperature",dht.readTemperature() );  // adding 'Temperature' value to the current payload
-   lo.addToPayload("Humidity", dht.readHumidity());// adding 'Humidity' value to the current payload
+  
+   if (millis() - lastMessageTime > messageRate) {
+    // collect data periodically("Sampling data");
+           h = dht.readHumidity();
+           // Read temperature as Celsius (the default)
+           t = dht.readTemperature(); 
+      while((isnan(h) || isnan(t))){
+           h = dht.readHumidity();
+           // Read temperature as Celsius (the default)
+           t = dht.readTemperature(); 
+      }
+   lo.addToPayload("Temperature",t );  // adding 'Temperature' value to the current payload
+   lo.addToPayload("Humidity", h);// adding 'Humidity' value to the current payload
    Serial.println("Sending data to Live Objects");
-   lo.sendData();                                   // send the data to Live Objects
-   lo.loop();                             // don't forget to keep this in your main loop
+   lo.sendData();  // send the data to Live Objects
+   lastMessageTime = millis();
+   }
+   lo.loop();   // don't forget to keep this in your main loop
+   
 }
